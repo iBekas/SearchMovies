d694974afa1f7c2841d3c0f2fd5972247d683588
iBekas <s345553452S>
2021-07-29 15:33:34 +0300

    Класс RepositoryImpl.kt переопределил методы из интерфейса RepositoryImpl.kt

--- app/src/main/java/search/finder/searchmovies/model/Movie.kt
+++ app/src/main/java/search/finder/searchmovies/model/Movie.kt
@@ -1,4 +1,3 @@
 package search.finder.searchmovies.model
 
-class Movie(val img: Int, val title: String = "Лучший фильм", val releaseYear: Int = 2020, val vote: Double = 4.5, val expectData:String = "01.01.2022") {
-}
+class Movie(val img: Int = 1, val title: String = "Лучший фильм", val releaseYear: Int = 2020, val vote: Double = 4.5, val expectData:String = "01.01.2022")
--- app/src/main/java/search/finder/searchmovies/model/RepositoryImpl.kt
+++ app/src/main/java/search/finder/searchmovies/model/RepositoryImpl.kt
@@ -1,4 +1,11 @@
 package search.finder.searchmovies.model
 
-class RepositoryImpl {
+class RepositoryImpl: Repository{
+    override fun getMovieFromServer(): Movie {
+        return Movie();
+    }
+
+    override fun getMovieFromLocal(): Movie {
+        return Movie();
+    }
 }
