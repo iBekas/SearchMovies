dd6610301ab44e107f01044ffdeeb3c9d38386fe
iBekas <s345553452S>
2021-07-29 16:31:42 +0300

    Toolbar

--- app/src/main/java/search/finder/searchmovies/view/MainActivity.kt
+++ app/src/main/java/search/finder/searchmovies/view/MainActivity.kt
@@ -1,7 +1,11 @@
 package search.finder.searchmovies.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import search.finder.searchmovies.R
 import search.finder.searchmovies.databinding.MainActivityBinding
 
@@ -19,5 +23,37 @@ class MainActivity : AppCompatActivity() {
 //            navigation.addFragment(NowPlayingFragment.newInstance(), R.id.now_playing_container, true)
             navigation.addFragment(UpcomingFragment.newInstance(), R.id.upcoming_movies_container, true)
         }
+        initToolbar()
     }
+
+    private fun initToolbar(): Toolbar {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        return toolbar
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.context_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.sort -> {
+                Toast.makeText(this, "Сортируем", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "Нстраиваем", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.favorite -> {
+                Toast.makeText(this, "Держи избранное", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
