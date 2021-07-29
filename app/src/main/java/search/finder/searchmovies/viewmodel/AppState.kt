9c275aa0be474747aa27ee814a8cc2209b15f0b5
iBekas <s345553452S>
2021-07-29 15:49:02 +0300

    Добавил состояния в AppState.kt

--- app/src/main/java/search/finder/searchmovies/viewmodel/AppState.kt
+++ app/src/main/java/search/finder/searchmovies/viewmodel/AppState.kt
@@ -1,4 +1,9 @@
 package search.finder.searchmovies.viewmodel
 
-class AppState {
+import search.finder.searchmovies.model.Movie
+
+sealed class AppState {
+    data class Success(val dataWeather: Movie):AppState()
+    data class Error( val error:Throwable):AppState()
+    object Loading: AppState()
 }
