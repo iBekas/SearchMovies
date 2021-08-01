db653e2edc2dbe83a6a987fe2d646a86fb527f53
iBekas <s345553452S>
2021-08-01 23:21:29 +0300

    Фрагмент информации о фильме v.1.0 добавлен

--- app/src/main/java/search/finder/searchmovies/view/MovieDetailFragment.kt
+++ app/src/main/java/search/finder/searchmovies/view/MovieDetailFragment.kt
@@ -1,30 +1,52 @@
 package search.finder.searchmovies.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import search.finder.searchmovies.R
+import androidx.fragment.app.Fragment
+import search.finder.searchmovies.databinding.FragmentMovieDetailBinding
+import search.finder.searchmovies.model.Movie
+
 
 class MovieDetailFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
-    }
-
     companion object {
-        fun newInstance(param1: String, param2: String){
+        const val KEY_MOVIE = "KEY"
+        fun newInstance(bundle: Bundle): MovieDetailFragment {
+            val fragment = MovieDetailFragment()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
 
+    private var _binding: FragmentMovieDetailBinding? = null
+    private val binding: FragmentMovieDetailBinding
+        get(): FragmentMovieDetailBinding {
+            return _binding!!
         }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val movie = arguments?.getParcelable(KEY_MOVIE) as? Movie
+        if (movie != null) setData(movie)
+    }
+
+    private fun setData(movie: Movie){
+        binding.movieTitleDetail.text = movie.title
+        binding.movieDescDetail.text = movie.movieDescription
+        binding.movieReleaseYearDetail.text = (movie.releaseYear ?: movie.expectData) as CharSequence?
+        //выше подсказка студии, ее стоит проверить
+        binding.movieVotesAverageDetail.text = if(movie.vote != null) movie.vote.toString() else "0.0"
     }
 }
