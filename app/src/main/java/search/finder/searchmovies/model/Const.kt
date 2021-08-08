811d5c0252de04a9b0eda28802bfcebe7eba092e
iBekas <s345553452S>
2021-08-08 22:22:43 +0300

    Добавил файл с ключами(временно открытый)

---
+++ app/src/main/java/search/finder/searchmovies/model/Const.kt
@@ -0,0 +1,6 @@
+package search.finder.searchmovies.model
+
+const val TMDB_API_URL_NOW_PLAYING = "https://api.themoviedb.org/3/movie/now_playing"
+const val TMDB_API_URL_UPCOMING = "https://api.themoviedb.org/3/movie/upcoming"
+const val TMDB_API_KEY_NAME = "MOVIES_TMDB"
+const val TMDB_API_KEY_VALUE = "b347852febaa77e3f1f5829f248dc4c7"
